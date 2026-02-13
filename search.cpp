1ada95f6fafec86d814ff344f8beb8db2ed2d46c
9Pranjal <167281485+9Pranjal@users.noreply.github.com>
2026-02-13 18:17:41 +0530

    Update search.cpp
    Updated variable names and formatting for consistency.

--- search.cpp
+++ search.cpp
@@ -1,35 +1,43 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main() {
-    int n, key, comparisons = 0;
-    cout << "Enter number of elements: ";
+
+    int n, key;
+    int comp=0;
+
+    cout << "enter number of elements: ";
     cin >> n;
 
     int arr[n];
-    cout << "Enter elements:\n";
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
 
-    cout << "Enter element to search: ";
+    cout << "enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "enter element to search: ";
     cin >> key;
 
     int position = -1;
 
-    for (int i = 0; i < n; i++) {
-        comparisons++;
-        if (arr[i] == key) {
-            position = i + 1;
+    for(int i = 0; i < n; i++) {
+        comp++
+
+        if(arr[i] == key) {
+            position = i + 1;  
             break;
         }
     }
 
-    if (position != -1)
-        cout << "Element found at position: " << position << endl;
-    else
-        cout << "Element not found.\n";
+    if(position != -1) {
+        cout << "element found at position: " << position << endl;
+    }
+    else {
+        cout << "element not found\n";
+    }
 
-    cout << "Number of comparisons made: " << comparisons << endl;
+    cout << "number of comparisons made: " << comparisons << endl;
 
     return 0;
 }
