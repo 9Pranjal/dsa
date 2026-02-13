8e3c51a77364d0095d803f3fa913c284be38e222
9Pranjal <167281485+9Pranjal@users.noreply.github.com>
2026-02-13 18:18:23 +0530

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -1,41 +1,35 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int pivot = arr[high];
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                swap(arr[i], arr[j]);
-            }
-        }
-        swap(arr[i + 1], arr[high]);
-        int pi = i + 1;
-
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
 int main() {
+
     int n;
-    cout << "Enter number of elements: ";
+
+    cout << "enter number of elements: ";
     cin >> n;
 
     int arr[n];
-    cout << "Enter elements:\n";
-    for (int i = 0; i < n; i++)
+
+    cout << "enter elements:\n";
+    for(int i = 0; i < n; i++) {
         cin >> arr[i];
+    }
 
-    quickSort(arr, 0, n - 1);
+    // Bubble Sort
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - 1 - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 
-    cout << "Sorted array:\n";
-    for (int i = 0; i < n; i++)
+    cout << "sorted array:\n";
+    for(int i = 0; i < n; i++) {
         cout << arr[i] << " ";
+    }
 
     return 0;
 }
